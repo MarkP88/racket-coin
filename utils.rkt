129489d6f2bec0c48ed20b84ecf5aee4bb050e29
Boro Sitnikovski <buritomath@gmail.com>
2018-06-19 22:43:02 +0200

    Make sure wallet struct uses hex strings for easier DB management (2h mark)

---
+++ utils.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(define ASCII-ZERO (char->integer #\0))
+
+;; [0-9A-Fa-f] -> Number from 0 to 15
+(define (hex-char->number c)
+  (if (char-numeric? c)
+      (- (char->integer c) ASCII-ZERO)
+      (match c
+        [(or #\a #\A) 10]
+        [(or #\b #\B) 11]
+        [(or #\c #\C) 12]
+        [(or #\d #\D) 13]
+        [(or #\e #\E) 14]
+        [(or #\f #\F) 15]
+        [_ (error 'hex-char->number "invalid hex char: ~a\n" c)])))
+
+
+(define (hex-string->bytes str) (list->bytes (hex-string->bytelist str)))
+(define (hex-string->bytelist str)
+  (with-input-from-string
+      str
+    (thunk
+     (let loop ()
+       (define c1 (read-char))
+       (define c2 (read-char))
+       (cond [(eof-object? c1) null]
+             [(eof-object? c2) (list (hex-char->number c1))]
+             [else (cons (+ (* (hex-char->number c1) 16)
+                            (hex-char->number c2))
+                         (loop))])))))
+
+(provide hex-string->bytes)
--- wallet.rkt
+++ wallet.rkt
@@ -1,34 +1,37 @@
 #lang racket
 (require crypto)
 (require crypto/libcrypto)
+(require crypto/all)
+(require "utils.rkt")
 
 (struct wallet (private-key public-key))
+
+; We need to use all crypto factories for converting the key between hex<->pk-key
+(use-all-factories!)
 
 (define (make-wallet)
   (letrec ([rsa-impl (get-pk 'rsa libcrypto-factory)]
            [privkey (generate-private-key rsa-impl '((nbits 512)))]
            [pubkey (pk-key->public-only-key privkey)])
-    (wallet privkey pubkey)))
+    (wallet (bytes->hex-string (pk-key->datum privkey 'PrivateKeyInfo))
+            (bytes->hex-string (pk-key->datum pubkey 'SubjectPublicKeyInfo)))))
 
 (define (generate-signature privkey sender recipient value)
-  (digest/sign privkey
+  (digest/sign (datum->pk-key (hex-string->bytes privkey) 'PrivateKeyInfo)
                'sha1
                (string->bytes/latin-1 (string-append sender recipient value))))
 
 (define (verify-signature pubkey sig sender recipient value)
-  (digest/verify pubkey
+  (digest/verify (datum->pk-key (hex-string->bytes pubkey) 'SubjectPublicKeyInfo)
                  'sha1
                  (string->bytes/latin-1 (string-append sender recipient value))
                  sig))
 
-(define (print-wallet wallet)
-  (printf "private: ~a\npublic: ~a\n"
-          (bytes->hex-string (pk-key->datum (wallet-private-key wallet) 'PrivateKeyInfo))
-          (bytes->hex-string (pk-key->datum (wallet-public-key wallet) 'SubjectPublicKeyInfo))))
-
+#|
 (define test-wallet (make-wallet))
 
 (define signature (generate-signature (wallet-private-key test-wallet) "test" "test" "test"))
 (verify-signature (wallet-public-key test-wallet) signature "test" "test" "test")
+|#
 
-(provide (struct-out wallet) make-wallet print-wallet)
+(provide (struct-out wallet) make-wallet generate-signature verify-signature)

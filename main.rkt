0cdef7798be8f13f75142cc23371794c627a9d27
Boro Sitnikovski <buritomath@gmail.com>
2018-06-25 17:27:21 +0200

    Update example to contain multiple transactions

--- main.rkt
+++ main.rkt
@@ -2,23 +2,30 @@
 (require "blockchain.rkt")
 (require "utils.rkt")
 
-; Wallet and transaction test
+; Initialize wallets
 (define scheme-coin-base (make-wallet))
 (define wallet-a (make-wallet))
 (define wallet-b (make-wallet))
 
-(define utxo (list
-              (make-transaction-io 100 wallet-a)))
-
+; Transactions
 (printf "Making genesis transaction...\n")
 (define genesis-t (make-transaction scheme-coin-base
                                     wallet-a
                                     100
                                     '()))
 
-; Blockchain test
+; Unspent transactions (store our genesis transaction)
+(define utxo (list
+              (make-transaction-io 100 wallet-a)))
+
+; Blockchain initiation
 (printf "Mining genesis block...\n")
-(define blockchain (init-blockchain genesis-t (string->bytes/utf-8 "seedgenesis") utxo))
+(define blockchain (init-blockchain
+                    genesis-t
+                    (string->bytes/utf-8 "seedgenesis")
+                    utxo))
+
+(newline)
 
 (printf "Wallet A balance: ~a\nWallet B balance: ~a\n"
         (balance-wallet-blockchain blockchain wallet-a)
@@ -26,8 +33,31 @@
 
 (newline)
 
-(printf "Mining transaction...\n")
-(set! blockchain (send-money-blockchain blockchain wallet-a wallet-b 50))
+; Make a second transaction
+(printf "Mining second transaction...\n")
+(set! blockchain (send-money-blockchain
+                  blockchain
+                  wallet-a
+                  wallet-b
+                  20))
+
+(newline)
+
+(printf "Wallet A balance: ~a\nWallet B balance: ~a\n"
+        (balance-wallet-blockchain blockchain wallet-a)
+        (balance-wallet-blockchain blockchain wallet-b))
+
+(newline)
+
+; Make a third transaction
+(printf "Mining third transaction...\n")
+(set! blockchain (send-money-blockchain
+                  blockchain
+                  wallet-b
+                  wallet-a
+                  10))
+
+(newline)
 
 (printf "Wallet A balance: ~a\nWallet B balance: ~a\n"
         (balance-wallet-blockchain blockchain wallet-a)

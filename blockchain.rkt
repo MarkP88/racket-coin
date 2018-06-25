899356ecf552ee6766d7d1021da591d0fd5c5c10
Boro Sitnikovski <buritomath@gmail.com>
2018-06-25 17:17:08 +0200

    Refactor blockchain structure for UTXO support

--- blockchain.rkt
+++ blockchain.rkt
@@ -3,28 +3,35 @@
 (require "transaction.rkt")
 (require "utils.rkt")
 (require "wallet.rkt")
+(require racket/serialize)
 
-(define (init-blockchain data seed-hash) (cons (mine-block data seed-hash) '()))
+(serializable-struct blockchain (blocks utxo))
 
-(define (add-blockchain blockchain data)
-  (let ([hashed-blockchain (mine-block data (block-hash (car blockchain)))])
-    (cons hashed-blockchain blockchain)))
+(define (init-blockchain t seed-hash utxo)
+  (blockchain (cons (mine-block (process-transaction t) seed-hash) '())
+              utxo))
 
-(define (valid-blockchain? blockchain)
-  (and
-   ; Compare calculated hashes
-   (true-for-all? valid-block? blockchain)
-   ; Compare previous hashes
-   (equal? (drop-right (map block-previous-hash blockchain) 1)
-           (cdr (map block-hash blockchain)))
-   ; Any data that is a transaction should be valid
-   (true-for-all?
-    (lambda (block)
-      (if (transaction? (block-transaction block)) (valid-transaction? (block-transaction block)) #t)) blockchain)
-   ; Check that block is mined
-   (true-for-all? mined-block? (map block-hash blockchain))))
+(define (add-blockchain b t)
+  (let ([hashed-blockchain (mine-block t (block-hash (car (blockchain-blocks b))))])
+    (blockchain (cons hashed-blockchain (blockchain-blocks b)) (blockchain-utxo b))))
+
+(define (valid-blockchain? b)
+  (let ([blocks (blockchain-blocks b)])
+    (and
+     ; Compare calculated hashes
+     (true-for-all? valid-block? blocks)
+     ; Compare previous hashes
+     (equal? (drop-right (map block-previous-hash blocks) 1)
+             (cdr (map block-hash blocks)))
+     ; Any data that is a transaction should be valid
+     (true-for-all?
+      (lambda (block)
+        (if (transaction? (block-transaction block)) (valid-transaction? (block-transaction block)) #t)) blocks)
+     ; Check that block is mined
+     (true-for-all? mined-block? (map block-hash blocks)))))
 
 (provide (all-from-out "block.rkt")
          (all-from-out "transaction.rkt")
          (all-from-out "wallet.rkt")
+         (struct-out blockchain)
          init-blockchain add-blockchain valid-blockchain?)
--- main.rkt
+++ main.rkt
@@ -3,6 +3,7 @@
 (require "utils.rkt")
 
 ; Wallet and transaction test
+(define scheme-coin-base (make-wallet))
 (define wallet-a (make-wallet))
 (define wallet-b (make-wallet))
 
@@ -10,9 +11,19 @@
 
 (printf "Transaction is valid: ~a\n" (valid-transaction? tr))
 
+(define utxo (list
+              (make-transaction-io 100 wallet-a)))
+
+(printf "Making genesis transaction...\n")
+(define genesis-t (make-transaction scheme-coin-base
+                                    wallet-a
+                                    100
+                                    '()))
+
+
 ; Blockchain test
 (printf "Mining genesis block...\n")
-(define blockchain (init-blockchain "Hello World" (string->bytes/utf-8 "seedgenesis")))
+(define blockchain (init-blockchain genesis-t (string->bytes/utf-8 "seedgenesis") utxo))
 (printf "Mining transaction...\n")
 (set! blockchain (add-blockchain blockchain tr))
 
@@ -20,6 +31,6 @@
 
 (newline)
 
-(for ([block blockchain])
+(for ([block (blockchain-blocks blockchain)])
   (print-block block)
   (newline))

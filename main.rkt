9fc7d7fdac477228be0ecaeb22702f75f6a1d687
Boro Sitnikovski <buritomath@gmail.com>
2018-06-27 10:03:26 +0200

    Separate main and main-helper (procedures) for readability

--- main.rkt
+++ main.rkt
@@ -1,31 +1,5 @@
 #lang racket
-(require "./src/blockchain.rkt")
-(require "./src/utils.rkt")
-(require (only-in sha bytes->hex-string))
-
-(define (format-transaction t)
-  (format "...~a... sends ...~a... an amount of ~a."
-          (substring (wallet-public-key (transaction-from t)) 64 80)
-          (substring (wallet-public-key (transaction-to t)) 64 80)
-          (transaction-value t)))
-
-(define (print-block block)
-  (printf "Block information\n=================\nHash:\t~a\nHash_p:\t~a\nStamp:\t~a\nNonce:\t~a\nData:\t~a\n"
-          (bytes->hex-string (block-hash block))
-          (bytes->hex-string (block-previous-hash block))
-          (block-timestamp block)
-          (block-nonce block)
-          (format-transaction (block-transaction block))))
-
-(define (print-blockchain b)
-  (for ([block (blockchain-blocks b)])
-    (print-block block)
-    (newline)))
-
-(define (print-wallets wallet-a wallet-b)
-  (printf "\nWallet A balance: ~a\nWallet B balance: ~a\n\n"
-          (balance-wallet-blockchain blockchain wallet-a)
-          (balance-wallet-blockchain blockchain wallet-b)))
+(require "./main-helper.rkt")
 
 (when (file-exists? "blockchain.data")
   (begin
@@ -49,22 +23,22 @@
 ; Blockchain initiation
 (printf "Mining genesis block...\n")
 (define blockchain (init-blockchain genesis-t (string->bytes/utf-8 "seedgenesis") utxo))
-(print-wallets wallet-a wallet-b)
+(print-wallets blockchain wallet-a wallet-b)
 
 ; Make a second transaction
 (printf "Mining second transaction...\n")
 (set! blockchain (send-money-blockchain blockchain wallet-a wallet-b 20))
-(print-wallets wallet-a wallet-b)
+(print-wallets blockchain wallet-a wallet-b)
 
 ; Make a third transaction
 (printf "Mining third transaction...\n")
 (set! blockchain (send-money-blockchain blockchain wallet-b wallet-a 10))
-(print-wallets wallet-a wallet-b)
+(print-wallets blockchain wallet-a wallet-b)
 
 ; Attempt to make a fourth transaction
 (printf "Attempting to mine fourth (not-valid) transaction...\n")
 (set! blockchain (send-money-blockchain blockchain wallet-b wallet-a 200))
-(print-wallets wallet-a wallet-b)
+(print-wallets blockchain wallet-a wallet-b)
 
 (printf "Blockchain is valid: ~a\n\n" (valid-blockchain? blockchain))
 
